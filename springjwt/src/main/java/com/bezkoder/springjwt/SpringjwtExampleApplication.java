341fd1cfb1436d48c90dea2b6ab7b7b84d6cb402
Jayesh Nachnani <jayesh.nachnani@gmail.com>
2021-08-08 19:52:31 +0530

    Lombok changes
    base files from :
    https://www.bezkoder.com/spring-boot-jwt-authentication/ 
    https://bezkoder.com/angular-12-spring-boot-jwt-auth/#comment-9220

---
+++ springjwt/src/main/java/com/bezkoder/springjwt/SpringjwtExampleApplication.java
@@ -0,0 +1,13 @@
+package com.bezkoder.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjwtExampleApplication.class, args);
+	}
+
+}
---
+++ springjwt/src/main/java/com/bezkoder/springjwt/models/ERole.java
@@ -0,0 +1,13 @@
+package com.bezkoder.springjwt.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
+
+//Notes: When to use an enum?
+//Build pattern
+// Why do we need PoJos for payload and request response?
+
+
